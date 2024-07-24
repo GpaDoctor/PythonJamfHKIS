16cc4e8d3ebf07896da1aa08141d7f0c7cd0e981
Lawrence <lawerance@MacNB-2404.local>
2024-07-24 15:45:37 +0800

    add

--- README.md
+++ README.md
@@ -25,13 +25,21 @@ run the main.py and you are good to go
 *Remarks
 - INCASE unable to refresh authentication. Restart VS Code completely and retype the authentication.
 - INCASE import does not work. Click the bottom right hand corner Python 3.12.4 64-bit. Choose Global, then choose back Recommened.
+- Beware of Captitalization. Especially in  with Pro(JPS_URL, JPS_USERNAME,JPS_PASSWORD) as pro: and with Classic(JPS_URL, JPS_USERNAME,JPS_PASSWORD) as classic:
 - Beware when using pro.py when a 405 error shows up
         #For example: in the pro.create_mdm_command
         # the endpoint actually have a problem. This has to be updated in the pro.py file
         # command click on the pro.create_mdm_command to see the pro.py file
         # change the endpoint to the desired endpoint in this case endpoint = "/api/preview/mdm/commands"
         # save the pro.py file and run the script
+        # the json dict for the parameter can be generated in the jamf classic api link
+        #simply go to the corresponding api, fill in the parameters, in language click on Python copy the dictionary under payload = ...
         # The management id can be found under the inventory, general, Jamf Pro Management ID: ...
+- Beware of endpoint deprecation. This means that the API developers have marked it as outdated and no longer recommend using it for new applications or integrations. This is often done when a newer, better, or more efficient endpoint replaces the deprecated one.
+        # This can be found in the jamf classic api link. Marked with a ! next to the corresponding api.
+        # It is also marked indise the pro.py library e.g.                 
+        "Pro.create_macos_managed_software_updates has been deprecated by Jamf "
+                "Pro v10.44.0."
 
 
 
@@ -41,4 +49,7 @@ source:
 - https://gitlab.com/cvtc/appleatcvtc/jps-api-wrapper/-/blob/main/README.md#retrieving-the-password-in-python-and-authenticating
 - https://jps-api-wrapper.readthedocs.io/en/stable/
 - https://developer.jamf.com/jamf-pro/reference/classic-api
-- https://jss.hkis.edu.hk:8443/api/doc/#/mdm/get_v1_mdm_commands
+- https://jss.hkis.edu.hk:8443/api/doc/#/mdm/get_v1_mdm_commands     # This is swagger UI
+- https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.4.10 # This is error message for reference
+- https://www.jamf.com/blog/howto-erase-all-content-and-settings-macos-redeployment/ #reference for erase device
+- https://community.jamf.com/t5/tech-thoughts/how-to-securely-manage-local-admin-passwords-with-jamf-pro-and/ba-p/289969 # for management id
--- reset_computer.py
+++ reset_computer.py
@@ -18,19 +18,63 @@ def reset(JPS_URL, JPS_USERNAME,JPS_PASSWORD):
         # command click on the pro.create_mdm_command to see the pro.py file
         # change the endpoint to the desired endpoint in this case endpoint = "/api/preview/mdm/commands"
         # save the pro.py file and run the script
+        # the json dict for the parameter can be generated in the jamf classic api link
+        # simply go to the corresponding api, fill in the parameters, in language click on Python copy the dictionary under payload = ...
         # The management id can be found under the inventory, general, Jamf Pro Management ID: ...
+
+        # pro.create_mdm_command(
+        #     {
+        #     "commandData": {
+        #         "commandType": "SETTINGS",
+        #         "deviceName": "THIS IS ONE OF THE BEST DEVICES IN THE WORLD"
+        #     },
+        #     "clientData": [
+        #         {
+        #         "managementId": "4d7203c7-6043-4dae-8341-265768a15e5f"
+        #         }
+        #     ]
+        #     }
+        # )
+        # pro.create_mdm_command(
+        #     {
+        #     "commandData": {
+        #         "commandType": "RESTART_DEVICE",
+        #         "rebuildKernelCache": False
+        #     },
+        #     "clientData": [
+        #         {
+        #         "managementId": "4d7203c7-6043-4dae-8341-265768a15e5f"
+        #         }
+        #     ]
+        #     }
+        # )
+
+        #leave this for now, since testing the update requires downgrading the macos
+        # pro.create_managed_software_updates_plan(
+
+        # {
+        #     "devices": [
+        #         {
+        #             "objectType": "COMPUTER",
+        #             "deviceId": "9564"
+        #         }
+        #     ],
+        #     "config": {
+        #         "updateAction": "DOWNLOAD_INSTALL_RESTART",
+        #         "versionType": "LATEST_MAJOR",
+        #         "specificVersion": "NO_SPECIFIC_VERSION"
+        #     }
+        # }
+        # )
 
         pro.create_mdm_command(
             {
-            "commandData": {
-                "commandType": "SETTINGS",
-                "deviceName": "hihi"
-            },
-            "clientData": [
-                {
-                "managementId": "4d7203c7-6043-4dae-8341-265768a15e5f"
-                }
-            ]
+                "commandData": {
+                    "commandType": "ERASE_DEVICE",
+                    "disallowProximitySetup": True,
+                    "pin": "123456"
+                },
+                "clientData": [{ "managementId": " 4d7203c7-6043-4dae-8341-265768a15e5f" }]
             }
         )
 
